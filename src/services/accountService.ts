c98940f6134761dab2693571e909bae4463da45e
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-25 01:27:50 +0900

    validation email and password format

--- src/services/accountService.ts
+++ src/services/accountService.ts
@@ -9,11 +9,22 @@ const AccountService = {
     signUp: async (user:userDto) => {
         const email = user.email.trim();
         const password = user.password.trim();
+
         if (email == '' || password == ''){
-            return basicResponse('빈 문자열입니다.',400)
-        };
-        return basicResponse("회원가입 TEST")
-    }
+            return basicResponse('빈 문자열입니다.',400);
+        } 
+        else if(!emailRegex.test(email)) {
+            return basicResponse('잘못된 이메일 형식입니다.',400);
+        }
+        else if(!passwordRegex.test(password)){
+            return basicResponse('비밀번호는 영어, 숫자, 특수문자 혼용 8자 이상이어야 합니다.',400);
+        } 
+        else{
+            // 회원가입 로직 추가
+            return resultResponse("test",200,{"name":"장병희","age":13});
+        }
+        
+    },
 }
 
 
