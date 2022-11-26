cb177c84b2e30fc2de28e1efb778c59c807b3482
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-26 21:04:21 +0900

    connect mongDB

--- app.ts
+++ app.ts
@@ -1,13 +1,23 @@
 import express from "express";
 import controller from "./src/controllers";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
+const {PORT, MONGO_URI} = process.env;
 
-const PORT=3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 app.use('/',controller);
+
+//CONNECTnpm TO MONGODB SERVER
+mongoose
+  .connect(MONGO_URI!)
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
 
 app.listen(PORT, ()=>{
     console.log("서버 가동");
