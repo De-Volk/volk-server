415e94ade685087a2ca3e917cc52417a31f0be0a
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-27 19:56:37 +0900

    [feat]: add reissue toekn API

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,11 @@
 import express, {Request,Response,Router} from "express";
 import account from "./accountControl";
+import jwtService from "../auth/jwtService";
 
 const router:Router = express.Router();
 
 // 정상 동작 확인
-router.get('/',(req:Request, res:Response)=>{
+router.get('/',jwtService.getUserInfoFromToken,(req:any, res:Response)=>{
     res.json({"status":200,"data":"정상 동작"})
 });
 
@@ -13,5 +14,8 @@ router.post('/signup',account.signUp);
 
 // 로그인
 router.post('/login',account.login);
+
+// 토큰 재발행
+router.post('/token',jwtService.reissueToken);
 
 export default router;
