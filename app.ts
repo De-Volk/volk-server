b6db001cb5611e43d4a0c3b992439cd9b047a044
JangByeongHui <jmkabc31@ajou.ac.kr>
2022-11-27 16:57:24 +0900

    [feat]: add controller

--- app.ts
+++ app.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import controller from "./src/controllers";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: '*', credentials: true }));
+app.use("/",controller);
 
 mongoose.connect('mongodb://localhost:27017/volk', {});
 const db = mongoose.connection;
